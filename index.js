0ee239948b2cd5e6b97b4510ee5d255c84a6ec75
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-06-06 11:58:11 +0200

    axios-rpc

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
+const express = require("express");
+const axios = require("axios");
+axios.defaults.baseURL = 'http://localhost:3000';
+const port = 3000
 
-var dogecoin = require('node-dogecoin')()
- 
-dogecoin.auth('alex', 'alex2')
- 
-dogecoin.getDifficulty(function() {
-        console.log(arguments);
+
+const payload = {
+            jsonrpc: "1.0",
+            id: 0,
+            method: "getblockchaininfo",
+            params: []    
+};
+
+var headers = {
+        'User-Agent':       'Super Agent/0.0.1',
+        'Content-Type':     'application/json-rpc',
+        'Accept':'application/json-rpc'
+}
+
+var auth ={ username: "a", password: "b"   }
+
+var head = { "headers" : headers, "auth" : auth};
+(async () => {
+        const response = await axios.post("http://127.0.0.1:22555", payload,head)
+        console.log(response.data)
+    })();
+const app = express()
+
+
+app.get('/', (req, res) => {
+      res.send('Hello World!')
 })
+
+app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+})
+/*
+axios({
+      method: 'post',
+      url: 'http://127.0.0.1:22555',
+        data: {
+              jsonrpc : '1.0',
+              id : 'curltest',
+              medthod : "getblockchaininfo",
+              params : []
+            },
+    
+      auth:auth,
+      headers : headers,
+
+
+    
+})
+.then(
+    (response) => { console.log(response)},
+    (error)    => { console.log(error)}
+
+);
+
+*/
